ba581a1e5d38b22ed07e92060f47ef842e0604b2
nbhat1 <neeraj.engg@gmail.com>
2017-04-16 14:01:30 -0700

    Commit captureScreenShot method.

--- POMwithfactory_UIAutomation/src/main/java/com/test/automation/POMwithfactory_UIAutomation/excelReader/excelReader.java
+++ POMwithfactory_UIAutomation/src/main/java/com/test/automation/POMwithfactory_UIAutomation/excelReader/excelReader.java
@@ -3,7 +3,11 @@ package com.test.automation.POMwithfactory_UIAutomation.excelReader;
 public class excelReader {
 	
 	public void getData(){
-		System.out.println("Test Data file");
+		System.out.println("Test Data file1");
+		
+		
+		
+		
 	}
 
 }
--- POMwithfactory_UIAutomation/src/main/java/com/test/automation/POMwithfactory_UIAutomation/screenShots/screenShots.java
+++ POMwithfactory_UIAutomation/src/main/java/com/test/automation/POMwithfactory_UIAutomation/screenShots/screenShots.java
@@ -1,5 +1,9 @@
 package com.test.automation.POMwithfactory_UIAutomation.screenShots;
 
 public class screenShots {
+	
+	public void captureScreenShots(){
+		System.out.println("This method will capture screenshots");
+	}
 
 }
